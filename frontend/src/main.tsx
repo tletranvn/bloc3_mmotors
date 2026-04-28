42475cba8288677b3199cbf9c1ffefdb36fa63ac
tletranvn <tletranvn@gmail.com>
2026-04-28 15:02:26 +0200

    Sprint3 : remove SentryTestController & cleanup debug console.log

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,6 @@ import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.tsx'
 
-console.log('SENTRY DSN:', import.meta.env.VITE_SENTRY_DSN)
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   sendDefaultPii: true,
