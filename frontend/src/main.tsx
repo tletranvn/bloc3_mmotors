8a74c4b1fd5d8f0890de36c459cc04c5447882b0
tletranvn <tletranvn@gmail.com>
2026-04-28 09:07:51 +0200

    Sprint3 : setup Sentry monitoring for backend & frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './context/AuthContext'
+import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.tsx'
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: true,
+  enableLogs: true,
+})
 
 const queryClient = new QueryClient()
 
